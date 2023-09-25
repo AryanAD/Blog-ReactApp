0e94a0ded40454ea6195ed95385016e568c5add8
AryanAD <aryanhohai@gmail.com>
2023-09-25 17:35:36 +0545

    blog

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Display from "./Display";
 
 function App() {
 	return (
 		<>
 			<h1>BLOG APP</h1>
+			<Display />
 		</>
 	);
 }
---
+++ blog/src/Display.jsx
@@ -0,0 +1,9 @@
+const Display = () => {
+	return (
+		<div>
+			<h1>Data</h1>
+		</div>
+	);
+};
+
+export default Display;
