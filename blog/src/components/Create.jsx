32a7e7c8236c04fc2b78d95358580ce6c8c5f90d
AryanAD <aryanhohai@gmail.com>
2023-09-27 17:49:21 +0545

    blog

--- blog/src/components/Create.jsx
+++ blog/src/components/Create.jsx
@@ -1,5 +1,4 @@
 import {
-	Avatar,
 	Box,
 	Button,
 	Checkbox,
@@ -25,70 +24,47 @@ const Create = () => {
 					flexDirection: "column",
 					alignItems: "center",
 				}}>
-				<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
 				<Typography
 					component="h1"
 					variant="h5">
-					Sign in
+					Create new Blog
 				</Typography>
 				<Box
 					component="form"
 					noValidate
 					sx={{ mt: 1 }}>
 					<TextField
+						id="standard-basic"
+						variant="standard"
 						margin="normal"
-						required
 						fullWidth
-						id="email"
-						label="Email Address"
-						name="email"
-						autoComplete="email"
-						autoFocus
+						label="Blog Title"
+						type="text"
 					/>
 					<TextField
+						id="standard-basic"
+						variant="standard"
 						margin="normal"
-						required
 						fullWidth
-						name="password"
-						label="Password"
-						type="password"
-						id="password"
-						autoComplete="current-password"
+						label="Description"
+						type="text"
 					/>
-					<FormControlLabel
-						control={
-							<Checkbox
-								value="remember"
-								color="primary"
-							/>
-						}
-						label="Remember me"
+					<TextField
+						id="standard-basic"
+						variant="standard"
+						margin="normal"
+						fullWidth
+						label="Add Cover Image"
+						type="image"
 					/>
+
 					<Button
 						type="submit"
 						fullWidth
 						variant="contained"
 						sx={{ mt: 3, mb: 2 }}>
-						Sign In
+						Add
 					</Button>
-					<Grid container>
-						<Grid
-							item
-							xs>
-							<Link
-								href="#"
-								variant="body2">
-								Forgot password?
-							</Link>
-						</Grid>
-						<Grid item>
-							<Link
-								href="#"
-								variant="body2">
-								{"Don't have an account? Sign Up"}
-							</Link>
-						</Grid>
-					</Grid>
 				</Box>
 			</Box>
 		</Container>
