4e4dd2517b7d07262d0517e9c2e2a5c4aa816853
AryanAD <aryanhohai@gmail.com>
2023-09-27 16:43:06 +0545

    blogclear

--- blog/src/layout/HomePage.jsx
+++ blog/src/layout/HomePage.jsx
@@ -1,7 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
 import Cards from "../components/Cards";
 import Modify from "../components/Modify";
 import ListDisplay from "../components/ListItems";
+import { FileCopyIcon, SaveIcon, PrintIcon, ShareIcon } from "@mui/icons-material";
 
 const HomePage = () => {
 	const homeBG = {
@@ -40,6 +41,13 @@ const HomePage = () => {
 		width: "100%",
 	};
 
+	const actions = [
+		{ icon: <FileCopyIcon />, name: "Copy" },
+		{ icon: <SaveIcon />, name: "Save" },
+		{ icon: <PrintIcon />, name: "Print" },
+		{ icon: <ShareIcon />, name: "Share" },
+	];
+
 	return (
 		<Box sx={homeBG}>
 			<Box sx={firstSection}>
@@ -52,6 +60,21 @@ const HomePage = () => {
 				<Box sx={rightSecond}>
 					<ListDisplay />
 				</Box>
+			</Box>
+
+			<Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
+				<SpeedDial
+					ariaLabel="SpeedDial basic example"
+					sx={{ position: "absolute", bottom: 16, right: 16 }}
+					icon={<SpeedDialIcon />}>
+					{actions.map((action) => (
+						<SpeedDialAction
+							key={action.name}
+							icon={action.icon}
+							tooltipTitle={action.name}
+						/>
+					))}
+				</SpeedDial>
 			</Box>
 		</Box>
 	);
