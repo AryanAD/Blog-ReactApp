7e8b37ecf168e4d6eeaaeaa6db4b2cdbdf34b7c4
AryanAD <aryanhohai@gmail.com>
2023-09-29 11:06:43 +0545

    blog

--- blog/src/components/Create.jsx
+++ blog/src/components/Create.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
 	Box,
 	Button,
@@ -6,14 +7,51 @@ import {
 	TextField,
 	Typography,
 } from "@mui/material";
+import axios from "axios";
+import { Navigate } from "react-router";
 
 const Create = () => {
+	const nav = Navigate;
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [file, setFile] = useState(null);
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const handleFormSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const formData = new FormData();
+			formData.append("title", title);
+			formData.append("description", description);
+			formData.append("image", file);
+
+			const response = await axios.post(
+				"http://localhost:3000/blogs",
+				formData
+			);
+
+			if (response.status === 200) {
+				setTitle("");
+				setDescription("");
+				setFile(null);
+				setErrorMessage("");
+				alert("Blog created successfully!");
+			} else {
+				setErrorMessage(response.data.message);
+			}
+		} catch (error) {
+			console.error(error);
+			setErrorMessage("An error occurred while creating the blog.");
+		}
+		nav("/");
+	};
+
 	return (
 		<Container component="main">
 			<CssBaseline />
 			<Box
 				sx={{
-					width: "1200px",
 					marginTop: 8,
 					display: "flex",
 					flexDirection: "column",
@@ -21,39 +59,36 @@ const Create = () => {
 				}}>
 				<Typography
 					component="h1"
-					variant="h4">
+					variant="h5">
 					Create new Blog
 				</Typography>
-				<Box
-					component="form"
-					noValidate
-					sx={{ mt: 1 }}>
+				<form onSubmit={handleFormSubmit}>
 					<TextField
-						id="standard-basic"
 						variant="outlined"
 						margin="normal"
 						fullWidth
 						label="Blog Title"
 						type="text"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
 					/>
 					<TextField
-						rows={8}
-						maxRows={20}
-						multiline="true"
-						id="standard-basic"
+						rows={16}
+						multiline
 						variant="outlined"
 						margin="normal"
 						fullWidth
 						label="Description"
 						type="text"
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
 					/>
-					<TextField
-						variant="standard"
-						margin="normal"
-						fullWidth
+					<input
 						type="file"
+						accept="image/*"
+						onChange={(e) => setFile(e.target.files[0])}
 					/>
-
+					{errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
 					<Button
 						type="submit"
 						fullWidth
@@ -61,7 +96,7 @@ const Create = () => {
 						sx={{ mt: 3, mb: 2 }}>
 						Add
 					</Button>
-				</Box>
+				</form>
 			</Box>
 		</Container>
 	);
